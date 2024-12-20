d843be39a306c60b1d028acb47c1cf98906d18c0
malamini31 <mohamedlaminef32@gmail.com>
2024-12-20 10:04:59 +0000

    premier commit

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream>
+using namespace std;
+int main() {
+//variable permettent de compter le nombre de place
+  int nbrePlace = 10;
+  // variable permettent de récupérer le choix de l'utilisateur
+  int choix; 
+  cout<<"**********Evaluation 2*************"<< endl << endl;
+  //partie à compléter à partir du 3.1
+
+  return 0;
+}
